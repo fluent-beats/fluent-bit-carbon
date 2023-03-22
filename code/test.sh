d99324e68afb4693c91b14e1e125b7756e9866cb
cadu.goncalves <cadu.goncalves@gmail.com>
2023-03-22 20:48:12 -0300

    Adjusted worflow to use correct FluentBit version

--- code/test.sh
+++ code/test.sh
@@ -6,4 +6,4 @@ cd $(dirname $0)/..
 docker run --rm \
       -p 8125:8125/udp \
       -v $(pwd)/code/build:/my_plugin \
-      fluent/fluent-bit:1.8.4 /fluent-bit/bin/fluent-bit -e /my_plugin/flb-in_carbon.so -i carbon -o stdout
+      fluent/fluent-bit:1.8.11 /fluent-bit/bin/fluent-bit -e /my_plugin/flb-in_carbon.so -i carbon -o stdout
